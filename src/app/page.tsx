6d4a29bf966daadaef64a914c2a316997763f1b5
codewithtabish <kashisultan099@gmail.com>
2026-06-23 02:31:41 +0500

    added in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HomePage = () => {
@@ -6,6 +7,10 @@ const HomePage = () => {
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat earum, perspiciatis asperiores magnam harum nostrum eaque explicabo laudantium accusantium temporibus ex illo optio recusandae beatae velit! Consequuntur asperiores nihil voluptates.
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod animi, est itaque enim laudantium rem quis accusamus iste, veritatis, rerum aspernatur qui nostrum eius quam autem exercitationem. Iste, ratione ea!
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod animi, est itaque enim laudantium rem quis accusamus iste, veritatis, rerum aspernatur qui nostrum eius quam autem exercitationem. Iste, ratione ea!
+
+        <Link href={'/about'}>
+      About Page
+      </Link>
 
     </div>
   )
