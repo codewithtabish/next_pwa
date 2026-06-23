f7aae6b8bbce1b9254749579b4ba1747357a705c
codewithtabish <kashisultan099@gmail.com>
2026-06-23 05:31:26 +0500

    updated about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AboutPage = () => {
   return (
-    <div className='bg-red-400'>
+    <div className=''>
         <h2 className='text-xl'>About Me</h2>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. A adipisci quasi ab dolore accusantium fuga illum sint vitae aut quis deserunt cum at doloremque ipsa, sequi quia maxime non aspernatur!
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. A adipisci quasi ab dolore accusantium fuga illum sint vitae aut quis deserunt cum at doloremque ipsa, sequi quia maxime non aspernatur!
