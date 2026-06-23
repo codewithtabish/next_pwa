62e3afa27150ac0d7e264a7749446d25da85eb78
codewithtabish <kashisultan099@gmail.com>
2026-06-23 11:37:53 +0500

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,25 +13,41 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "talha tabish",
-  description: "pro developer",
-  manifest:'/manifest.json',
-  icons:{
-    icon:"/favicon.png"
-  },
-  openGraph:{
-        title: "CodeWithTabish",
-    description: "Learn Web Development, Mobile Development, DevOps and AI with Tabish.",
-    images: ["/opengraph-image.jpg"],
-  },
-  twitter:{
-        card: "summary_large_image",
-    title: "CodeWithTabish",
-    description: "Learn Web Development, Mobile Development, DevOps and AI with Tabish.",
-    images: ["/twitter-image.jpg"],
 
-  }
+export const metadata: Metadata = {
+  title: "CodeWithTabish - Learn Web Development, Mobile Dev, DevOps & AI",
+  description:
+    "Learn Web Development, Mobile App Development, DevOps, and AI Engineering with CodeWithTabish. Build real-world projects and become job-ready.",
+
+  manifest: "/manifest.json",
+
+  icons: {
+    icon: "/favicon.png",
+  },
+
+  openGraph: {
+    title: "CodeWithTabish",
+    description:
+      "Learn Web Development, Mobile App Development, DevOps and AI Engineering with CodeWithTabish. Build real-world projects.",
+    siteName: "CodeWithTabish",
+    type: "website",
+    images: [
+      {
+        url: "/opengraph-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "CodeWithTabish - Web Development & AI Learning Platform",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "CodeWithTabish",
+    description:
+      "Learn Web Development, Mobile App Development, DevOps and AI Engineering with CodeWithTabish.",
+    images: ["/twitter-image.jpg"],
+  },
 };
 
 export default async function RootLayout({children,params}:LayoutProps) {
